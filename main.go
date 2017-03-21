3e35089da1b38402db91b9931d30e5bfa3e35543
Alexander Brandstedt <alexanderbrandstedt@gmail.com>
2017-03-21 15:11:26 +0100

    change to use fmt not log change to check if flags are used if not exit 1

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"os"
 
 	kingpin "gopkg.in/alecthomas/kingpin.v2"
@@ -12,17 +12,22 @@ var Version = "not set"
 
 func main() {
 	version := kingpin.Flag("version", "Show Version").Bool()
-	all := kingpin.Flag("all", "Prune all container/images/volumes not used").Short('a').Bool()
+	all := kingpin.Flag("all", "Prune all container/images/volumes not used").Bool()
 	containers := kingpin.Flag("containers", "Prune all container not used").Short('c').Bool()
 	images := kingpin.Flag("images", "Prune all images not used").Short('i').Bool()
 	volumes := kingpin.Flag("volumes", "Prune all volumes not used").Short('v').Bool()
 	kingpin.Parse()
 	if *version {
-		log.Printf("Version: %s\n", Version)
+		fmt.Printf("Version: %s\n", Version)
 		os.Exit(0)
+	}
+	if !*containers && !*images && !*volumes && !*all {
+		fmt.Printf("No thing to remove. check the flags with --help\n")
+		os.Exit(1)
 	}
 
 	var cleaner Cleaner
+
 	if *all {
 		cleaner.unUsedVolumes = true
 		cleaner.stoppedContainers = true
@@ -34,6 +39,6 @@ func main() {
 	}
 	err := cleaner.CleanUp()
 	if err != nil {
-		log.Printf("%v", err)
+		fmt.Printf("%v\n", err)
 	}
 }
